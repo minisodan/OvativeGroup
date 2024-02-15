5385add80368ba1742d877bf7e6786b3ef8c45cf
Veritated <emily.anderson.11@ndsu.edu>
2024-02-15 11:07:44 -0600

    Updated main.py to work:

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-capt
 img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg'
 raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
-# conditional image captioning
+# Conditional image captioning
 text = "a photography of"
 inputs = processor(raw_image, text, return_tensors="pt")
 
-out = model.generate(inputs)
+out = model.generate(**inputs)
 print(processor.decode(out[0], skip_special_tokens=True))
 
-# unconditional image captioning
-inputs = processor(raw_image, return_tensors="pt")
+# Unconditional image captioning
+inputs = processor(images=raw_image, return_tensors="pt")
 
-out = model.generate(inputs)
+out = model.generate(**inputs)
 print(processor.decode(out[0], skip_special_tokens=True))
