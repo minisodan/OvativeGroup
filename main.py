feccb2db60e62fdcaeb5fd6fade3d0e8dd2004a6
Veritated <emily.anderson.11@ndsu.edu>
2024-02-20 11:36:04 -0600

    Local directory no intake

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import requests
+import torch
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
-print('What is the url of the image?')
-filename = input()
-
+# print('What is the url of the image?')
+# filename = input()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+raw_image = Image.open('C:/Users/emand/Downloads/ad3.jpg').convert("RGB")
 
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
 img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg'
-raw_image = Image.open(requests.get(filename, stream=True).raw).convert('RGB')
+# raw_image = Image.open(requests.get(filename, stream=True).raw).convert('RGB')
 # Conditional image captioning
 text = "a photography of"
 inputs = processor(raw_image, text, return_tensors="pt")
