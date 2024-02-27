0feb909bffb2285927929034b16944f04a27bf68
KingPhilip14 <kingian404@gmail.com>
2024-02-27 12:40:42 -0600

    Directory handling
    App can now take a directory of images and caption them

--- application/image_caption_generator.py
+++ application/image_caption_generator.py
@@ -3,15 +3,15 @@ from application.image_processor import ImageProcessor
 
 
 def process_input(user_input: str) -> None:
-    image_processing = ImageProcessor(user_input)
-    image_processing.validate_file_extension()
+    ImageProcessor(user_input).process_input()
     return
 
 
 def start():
-    print('Please provide an image URL(s) or directory path to an individual image. If submitting '
-          'multiple URLs, separate them with a comma and a space (i.e, ", ").')
-    user_input: str = input('> ')
+    print('Please provide an image URL(s) or a directory path to an image(s). If submitting '
+          'multiple URLs, separate them with a comma and a space (i.e, ", ").\n(You may quit the application at '
+          'anytime by entering "q" or "quit")')
+    user_input: str = input('\n> ')
 
     if utils.quitting(user_input):
         utils.end()
@@ -23,8 +23,9 @@ def start():
 
 
 def prompt_again() -> None:
-    print('Would you like to provide more images? (y/n)')
-    user_input = input('> ')
+    print('\nWould you like to provide more images? (y/n)')
+    user_input = input('\n> ')
+
     if user_input.lower() in ['y', 'yes']:
         utils.clear()
         start()
--- application/image_processor.py
+++ application/image_processor.py
@@ -26,43 +26,87 @@ def caption_image(img: Image, img_source: str, processor, model) -> None:
     unc_caption: str = 'Unconditional image caption: ' + processor.decode(out[0], skip_special_tokens=True)
     print(unc_caption, end='\n\n')
 
-    store_image(img_source, con_caption, unc_caption)
+    store_image(create_dir(), img_source, con_caption, unc_caption)
 
 
-def store_image(img_source: str, con_caption: str, unc_caption: str) -> None:
+def store_image(path: str, img_source: str, con_caption: str, unc_caption: str) -> None:
     """
     Stores the given captions of the given image in a file. Creates a new folder if it doesn't exist.
+    :param path: The path of the directory
     :param img_source: The URL or directory path that sourced the image
     :param con_caption: Conditional caption
     :param unc_caption: Unconditional caption
     :return: None
     """
-    parent_dir: str = os.path.expanduser('~') + '\\Desktop'  # User's directory (e.g., C:/Users/example_user_name/)
-    directory: str = 'Ovative Group Caption Generator'
-    path: str = os.path.join(parent_dir, directory)
-
-    try:
-        os.makedirs(path)  # make directory if it doesn't exist already
-        print(f'Created new directory, "{path}", to store generated captions.\n')
-    except OSError:
-        print(f'Directory "{path}" already exists. Creating file to store generated captions...\n')
 
     now: datetime = datetime.datetime.now()
     file_name: str = now.strftime("%Y-%m-%d %H-%M-%S")
 
-    with open(path + '\\' + file_name + '.txt', 'a') as file:
+    with open(os.path.join(path, '.txt'), 'a') as file:
         file.write(f'Image source: {img_source}\n'
                    f'{con_caption}\n'
                    f'{unc_caption}\n\n')
-        print(f'Stored output in "{path}\\' + file_name + '.txt"\n')
+        print(f'Stored output in "{path}\\' + file_name + '.txt"')
+
+
+def create_dir() -> str:
+    """
+    Creates the directory the inputs will be stored in and return that directory string.
+    :return: string representing the created folder directory
+    """
+
+    # User's directory (e.g., C:/Users/example_user_name/)
+    parent_dir: str = os.path.join(os.path.expanduser('~'), 'Desktop')
+    directory: str = 'Ovative Group Caption Generator'
+    path: str = os.path.join(parent_dir, directory)
+
+    if not os.path.exists(path):
+        utils.clear()
+        os.makedirs(path)  # make directory if it doesn't exist already
+        print(f'Created new directory, "{path}", to store generated captions.\n')
+
+    return path
+
+
+def valid_extension(img_source) -> bool:
+    """
+    This method will check if the user's input contains a valid file extension.
+    :return: True or False
+    """
+
+    # reverse the string, get the file extension, and then re-reverse the string
+    extension: str = img_source[::-1].split('.')[0][::-1]
+
+    result: bool = extension in ['jpeg', 'jpg', 'png', 'tiff', 'raw', 'webp']
+
+    # check if the file extension is valid
+    return result
+
+
+def invalid_msg(img_source) -> None:
+    print(f'\nThe given file, "{img_source}", is an invalid input. This will not be processed.')
+
+
+def break_line():
+    print('\n-----------------------------------------------------------------------')
+
+
+def prune(images: list[tuple[Image, str]]) -> list[tuple[Image, str]]:
+    """
+    This method will remove any None values from the list of image, image source pairings given.
+    :param images:
+    :return: a list without None values
+    """
+    return [image for image in images if image is not None]
 
 
 class ImageProcessor:
     def __init__(self, user_input: str):
         self.user_input: str = user_input
         self.urls: list[str] = []
+        self.path: str = create_dir()
 
-    def convert_input(self) -> list[tuple[Image, str]] | None:
+    def convert_input(self) -> list[tuple[Image, str]]:
         """
         This method converts the URL to an image if it is a valid image link. Otherwise, it converts the given file
         directory.
@@ -72,17 +116,19 @@ class ImageProcessor:
         # Do not delete this line; used for creating the image
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        print('\nChecking input...')
+        utils.clear()
+
+        print('Checking input...')
 
         if self.is_url():
-            self.clean_url()
-
-            # return a list of tuple of (Image, URL)
-            return [(Image.open(requests.get(url, stream=True).raw).convert('RGB'), url) for url in self.urls]
+            return [(Image.open(requests.get(img_source, stream=True).raw).convert('RGB'), img_source) if
+                    valid_extension(img_source) else invalid_msg(img_source) for img_source in self.urls]
 
         if self.is_dir():
             # return a list of tuple of (Image, directory)
-            return [(Image.open(self.user_input).convert("RGB"), self.user_input)]
+            return [(Image.open(os.path.join(self.user_input, img_source)).convert("RGB"),
+                    os.path.join(self.user_input, os.path.basename(img_source))) if valid_extension(img_source) else
+                    invalid_msg(img_source) for img_source in os.listdir(self.user_input)]
 
     def process_input(self) -> None:
         """
@@ -93,59 +139,22 @@ class ImageProcessor:
         processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
         model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
-        # Receive the given input as a Pillow Image object
+        # Receive the given input as a Pillow Image object and the given source
         images: list[tuple[Image, str]] | None = self.convert_input()
 
-        if images is None:
+        try:
+            # clean the input of any bad files
+            images = prune(images)
+        except TypeError:
             self.invalid_prompt()
-            return
 
-        print('\nValid input received. Processing image(s)...')
-        utils.clear()
+        print('\nProcessing...')
 
         for img, img_source in images:
             print()  # to space the captions generated in the terminal
 
-            try:
-                caption_image(img, img_source, processor, model)
-            except Exception as e:
-                print(e)
-                print('An error occurred while processing an image. Please try again.\n')
-
-    def valid_extension(self) -> bool:
-        """
-        This method will check if the user's input contains a valid file extension.
-        :return: True or False
-        """
-
-        # reverse the string, get the file extension, and then re-reverse the string
-        extension: str = self.user_input[::-1].split('.')[0][::-1]
-
-        # check if the file extension is valid
-        return extension in ['jpeg', 'jpg', 'png', 'tiff', 'raw', 'webp', ]
-
-    def validate_file_extension(self) -> None:
-        """
-        This method will check if the user's input contains a valid file extension. If not, it will prompt the user
-        again to enter a URL or file directory to an image.
-        :return: None
-        """
-
-        valid: bool = self.valid_extension()
-
-        while not valid:
-            utils.clear()
-            print(f'Invalid file extension, "{self.user_input}", was given. Please provide input containing a valid '
-                  f'file extension (jpeg, jpg, png, tiff, raw, webp).')
-            self.user_input = input('Please provide the new image url/directory. Press "Q" to quit.\n> ')
-
-            # end application if quitting
-            if utils.quitting(self.user_input):
-                utils.end()
-
-            valid: bool = self.valid_extension()
-
-        self.process_input()
+            caption_image(img, img_source, processor, model)
+            break_line()
 
     def is_url(self) -> bool:
         """
@@ -183,10 +192,9 @@ class ImageProcessor:
 
     def invalid_prompt(self) -> None:
         utils.clear()
-        print(f'Invalid input, "{self.user_input}", was given. Please provide an image URL or an existing directory.')
-        self.user_input = input('> ')
+        print(f'Invalid input, "{self.user_input}", was given. Please provide an image URL(s) or an existing directory '
+              f'to multiple images.')
+        self.user_input = input('\n> ')
 
         if not utils.quitting(self.user_input):
             self.process_input()
-
-        utils.end()
