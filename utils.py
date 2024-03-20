26aec5618b0156fe328eb479d07655e9286c9d1a
KingPhilip14 <kingian404@gmail.com>
2024-03-20 17:25:50 -0500

    Enforced a singleton structure and slightly changed the input loop.

--- utils.py
+++ utils.py
@@ -18,3 +18,41 @@ def end() -> None:
     clear()
     print('Ending program.')
     sys.exit()
+
+
+def is_dir(user_input: str) -> bool:
+    """
+    This method checks if the user's input is an existing, local directory.
+    :return: True or False
+    """
+
+    return os.path.exists(user_input)
+
+
+def clean_url(user_input: str) -> str:
+    """
+    If the given URL contains 'www,' add 'https://' to the beginning of the string so the model recognizes it.
+    :return: None
+    """
+
+    if user_input[0:3] == 'www':
+        return 'https://' + user_input
+
+
+def create_dir() -> str:
+    """
+    Creates the directory the inputs will be stored in and return that directory string.
+    :return: string representing the created folder directory
+    """
+
+    # User's directory (e.g., C:/Users/example_user_name/)
+    parent_dir: str = os.path.join(os.path.expanduser('~'), 'Desktop')
+    directory: str = 'Ovative Group Caption Generator'
+    path: str = os.path.join(parent_dir, directory)
+
+    if not os.path.exists(path):
+        clear()
+        os.makedirs(path)  # make directory if it doesn't exist already
+        print(f'Created new directory, "{path}", to store generated captions.\n')
+
+    return path
