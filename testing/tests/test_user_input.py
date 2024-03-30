e44dab1f06fe9539933b7813717ae0cccf72f083
Mason Myles <mason.myles98@gmail.com>
2024-03-30 00:28:27 -0500

    i want mimi's

--- testing/tests/test_user_input.py
+++ testing/tests/test_user_input.py
@@ -1,16 +1,51 @@
-import io
 import unittest
-from unittest.mock import Mock
-import app.image_caption_generator as icg
+from unittest.mock import patch, MagicMock
+from PIL import Image
+from io import BytesIO
+from app.image_processor import ImageProcessor
 
 
-class TestUserInput(unittest.TestCase):
-    def setUp(self) -> None:
-        self.icg = icg.ImageProcessor
+class TestImageProcessor(unittest.TestCase):
 
-    def test_process_input_successful(self) -> None:
-        mock: Mock = Mock()
-        icg.process_input = mock
-        icg.process_input("https://i.pinimg.com/736x/8f/8c/f2/8f8cf2b9a584100047ff16974a5464e8.jpg")
+    @patch('app.image_processor.utils.is_url', return_value=True)
+    @patch('app.image_processor.ImageProcessor.__source_to_image_object')
+    def test_source_to_image_object_url(self, mock_source_to_image_object, mock_is_url):
+        # Mocking __source_to_image_object method
+        img_url = "https://example.com/image.jpg"
+        mock_source_to_image_object.return_value = [(Image.new('RGB', (100, 100)), img_url)]
 
-        mock.assert_called_once()
+        processor = ImageProcessor()
+        result = processor._ImageProcessor__source_to_image_object([img_url])
+
+        self.assertEqual(len(result), 1)
+        self.assertIsInstance(result[0][0], Image.Image)
+        self.assertEqual(result[0][1], img_url)
+
+    @patch('app.image_processor.utils.is_dir', return_value=True)
+    @patch('app.image_processor.ImageProcessor.__source_to_image_object')
+    def test_source_to_image_object_dir(self, mock_source_to_image_object, mock_is_dir):
+        # Mocking __source_to_image_object method
+        img_dir = "/path/to/image/directory"
+        mock_source_to_image_object.return_value = [(Image.new('RGB', (100, 100)), img_dir)]
+
+        processor = ImageProcessor()
+        result = processor._ImageProcessor__source_to_image_object([img_dir])
+
+        self.assertEqual(len(result), 1)
+        self.assertIsInstance(result[0][0], Image.Image)
+        self.assertEqual(result[0][1], img_dir)
+
+    @patch('app.image_processor.utils.is_dir', return_value=True)
+    @patch('PIL.Image.open')
+    def test_source_to_image_object_dir_permission_error(self, mock_open, mock_is_dir):
+        # Mocking image source as a directory with permission error
+        img_dir = "/path/to/image/directory"
+        mock_open.side_effect = PermissionError()
+
+        processor = ImageProcessor()
+        with self.assertRaises(SystemExit):
+            processor._ImageProcessor__source_to_image_object([img_dir])
+
+
+if __name__ == '__main__':
+    unittest.main()
